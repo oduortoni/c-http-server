a7a35e6c6257b8b9e006dfc100677d6030fe8b49
Ruslan Kovtun <koutoftimer@gmail.com>
2026-02-05 00:24:51 +0200

    Fixes rebase issue

--- src/lib/http/http_server.c
+++ src/lib/http/http_server.c
@@ -12,7 +12,7 @@ int listenAndServe(char *host, Router *router)
     router = http.router;
   }
   puts("http/server.c");
-  for (int i = 0; i < 4; i++)
+  for (size_t i = 0; i < ARRAY_LEN(router->patterns) && router->patterns[i]; i++)
   {
     printf("SERVERT: %s\n", router->patterns[i]);
   }
