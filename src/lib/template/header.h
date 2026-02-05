4eba26f0073693e2307358cf1debf51a196c8799
oduortoni <oduortoni@gmail.com>
2026-02-05 15:26:57 +0300

    fix(formatting): reduce the length of comments

--- src/lib/template/header.h
+++ src/lib/template/header.h
@@ -18,15 +18,17 @@
  * Loads a template file into memory.
  * @param filename The path to the template file.
  * @return A pointer to the loaded template content, or NULL on failure.
- *         The caller is responsible for freeing the returned pointer using free_template().
+ *         The caller is responsible for freeing the returned pointer
+ *          using free_template.
  */
 char* template_load(const char* filename);
 
 /**
  * template_free
  * Frees the memory allocated for a template.
- * @param template A pointer to the template content previously loaded with load_template().
+ * @param template A pointer to the template content previously loaded
+ *          with load_template().
  */
 void template_free(char* template);
 
-#endif // _TEMPLATE_HEADER_H
+#endif  // _TEMPLATE_HEADER_H
