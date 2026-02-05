b59ec42492b4f1ae8a1b5ad682a4732f57cdf9d6
oduortoni <oduortoni@gmail.com>
2026-02-05 14:48:47 +0300

    feat: add template loading system
    - Add template_loader.c with template_load and template_free functions
    - Add header.h with function declarations for template operations

---
+++ src/lib/template/header.h
@@ -0,0 +1,32 @@
+#ifndef _TEMPLATE_HEADER_H
+#define _TEMPLATE_HEADER_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Maximum allowed size for a template file.
+ * Used to prevent excessive memory usage.
+ * Approximately 512 KiB.
+ */
+#define MAX_TEMPLATE_SIZE (512 * 1024)
+
+/**
+ * template_load
+ * Loads a template file into memory.
+ * @param filename The path to the template file.
+ * @return A pointer to the loaded template content, or NULL on failure.
+ *         The caller is responsible for freeing the returned pointer using free_template().
+ */
+char* template_load(const char* filename);
+
+/**
+ * template_free
+ * Frees the memory allocated for a template.
+ * @param template A pointer to the template content previously loaded with load_template().
+ */
+void template_free(char* template);
+
+#endif // _TEMPLATE_HEADER_H
---
+++ src/lib/template/template_loader.c
@@ -0,0 +1,60 @@
+#include "header.h"
+
+#define READ_CHUNK 4096
+
+char *template_load(const char *filename)
+{
+    FILE *file = fopen(filename, "rb");
+    if (!file)
+        return NULL;
+
+    char *buffer = NULL;
+    size_t size = 0;
+    size_t capacity = 0;
+
+    for (;;) {
+        if (size + READ_CHUNK > MAX_TEMPLATE_SIZE) {
+            errno = EFBIG;
+            goto fail;
+        }
+
+        if (size + READ_CHUNK + 1 > capacity) {
+            size_t new_capacity = capacity ? capacity * 2 : READ_CHUNK;
+            if (new_capacity > MAX_TEMPLATE_SIZE + 1)
+                new_capacity = MAX_TEMPLATE_SIZE + 1;
+
+            char *tmp = realloc(buffer, new_capacity);
+            if (!tmp)
+                goto fail;
+
+            buffer = tmp;
+            capacity = new_capacity;
+        }
+
+        size_t n = fread(buffer + size, 1, READ_CHUNK, file);
+        size += n;
+
+        if (n < READ_CHUNK) {
+            if (ferror(file))
+                goto fail;
+            break;
+        }
+    }
+
+    buffer[size] = '\0';
+    fclose(file);
+    return buffer;
+
+fail:
+    /**
+     * Cleanup on failure
+     */
+    free(buffer);
+    fclose(file);
+    return NULL;
+}
+
+void template_free(char *template)
+{
+    free(template);
+}
